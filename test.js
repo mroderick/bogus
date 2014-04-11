f562f7eeb63cefe0322825ddbfa50fde7094ae6c
Morgan Roderick <morgan@roderick.dk>
2014-04-11 15:20:19 +0200

    Initial import

---
+++ test.js
@@ -0,0 +1,28 @@
+'use strict';
+var requirejs = require('requirejs');
+requirejs.config({
+    baseUrl: '.',
+    nodeRequire: require
+});
+
+describe('Something', function(){
+    var foo;
+
+    before(function (done){
+        // This saves the module foo for use in tests. You have to use
+        // the done callback because this is asynchronous.
+        requirejs(['foo'],
+                  function(mod) {
+            console.log("fired!");
+            foo = mod;
+            done();
+        });
+    });
+
+  describe('blah', function(){
+    it('blah', function(){
+      if (foo.test !==  "test")
+          throw new Error("failed!");
+    });
+  });
+});
